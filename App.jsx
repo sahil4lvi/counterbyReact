427b4b2bd015da73e5c6b16053d643df804a508c
sahil4lvi <lostsmoke05@gmail.com>
2025-07-11 15:26:53 +0530

    color changer page

--- App.jsx
+++ App.jsx
@@ -1,82 +1,24 @@
-import { useState, useCallback} from 'react'
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 
 function App() {
-  const [length, setLength] = useState(8)
-  const [numberAllowed, setNumberAllowed] = useState(false);
-  const [charAllowed, setcharAllowed] = useState(false)
-  const [ password, setpassword] = useState("")
-
-
-
-  const passwordGenerator = useCallback(()=> {
-    let pass = "";
-    let char = "abcdefghijklmnopqrstuvwxyz";
-    if (numberAllowed) char += "0123456789";
-    if (charAllowed) char += "!@#$%^&*()_";
-
-    for (let i = 1; index <length; i++) {
-     let random = math.floor(math.random() * char.length + 1);
-      pass = char.charAt(random)
-    }
-      setpassword(pass)
+  const [color, setColor] = useState('olive')
+  return(
+    <div style={{background: color, color: "white" , width: "100%", height: "10000px"}}>
+     <div style={{backgroundColor:"#000", color: "white", width: "100%", height: "50px", justifyContent: "center"}}>
     
-  }, [length, numberAllowed, charAllowed, setpassword])
- 
-  
-
-return(
-<>
-<h1 style={{color:"white", marginLeft:"500px"}} >password generator</h1>
-<div style={{backgroundColor:"grey" , height:"200px", borderRadius:"20px", width:"100%"}}>
-  <div>
-    <input
-     type="text"
-     value={password}
-     style={{width:"80%", height:"50px", backgroundColor:"white", borderRadius:"20px"}}
-     placeholder='password'
-     readOnly
-     />
-     <button style={{background:"black", color:"white", borderRadius:"10px"}}>copy</button>
-  </div>
-  <div>
-    <div>
-      <input
-       type="range" 
-       min={5}
-       max={20}
-       value={length}
-       className='curser-pointer'
-       onChange={(e) => setLength(e.target.value)}
-      />
-      <label>length: {length}</label>
+    <button style={{backgroundColor:"red", borderRadius:"10px", border:"5px", color:"white"}} onClick={() => setColor('linear-gradient(to right, red, blue)')}>costom</button>
+    
+      <button  style={{backgroundColor:"red", borderRadius:"10px", border:"5px", color:"white"}} onClick={() => setColor('red')}>red</button>
+      <button  style={{backgroundColor:"green", borderRadius:"10px", border:"5px", color:"white"}} onClick={() => setColor('green')}>green</button>
+      <button style={{backgroundColor:"blue", borderRadius:"10px", border:"5px", color:"white"}} onClick={() => setColor('skyblue')}>blue</button>
+      <button style={{backgroundColor:"brown", borderRadius:"10px", border:"5px", color:"white"}} onClick={() => setColor('brown')}>brown</button>
+      <button style={{backgroundColor:"black", borderRadius:"10px", border:"5px", color:"white"}} onClick={() => setColor('black')}>black</button>
+      <button style={{backgroundColor:"purple", borderRadius:"10px", border:"5px", color:"white"}} onClick={() => setColor('violet')}>random</button>
+     </div>
     </div>
-    <div>
-    <input
-       type="checkbox" 
-       defaultChecked={numberAllowed}
-       id='numberInput'
-      onChange={() => { setNumberAllowed((prev) => !prev)
-     
-      }}
-      />
-       <label htmlFor="numberinput">numbers</label>
-    </div>
-    <div>
-    <input
-       type="checkbox" 
-       defaultChecked={charAllowed}
-       id='numberInput'
-      onChange={() => { setcharAllowed((prev) => !prev)
-     
-      }}
-      />
-       <label htmlFor="numberinput">charactors</label>
-    </div>
-  </div>
-</div>
-</>
 )
+
 }
 
 export default App
